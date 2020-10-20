3fe619234758483b83aaffca14775e07f7b0603c
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-10-20 00:49:43 -0400

    added map to modal and retrieved product data in modal. need styling for button and modal

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function showPosition(position) {
   localStorage.setItem("lon", JSON.stringify(position.coords.longitude));
   GetMap();
 }
-getLocation();
+// getLocation();
 
 var APIKey = "&rapidapi-key=092293fd7emshf39e0f6436d8314p1ad470jsnee116871f2d1";
 // $(document).ready(function() {
@@ -100,11 +100,17 @@ function GetMap() {
     }
 }
 
-$("#searchButton").on("click", function () {
-  var queryURL = "https://makeup.p.rapidapi.com/products.json?";
-  var makeup = ""; //$("#searchField").val();
-  var type = "lipstick"; //$("#type").val();
+// var type = "";
+// $(".button").on("click", function() {
+//   type = $(this).text();
+// })
 
+$("#searchButton").on("click", function () {
+  var queryURL = "http://makeup-api.herokuapp.com/api/v1/products.json?";
+  var makeup = $("#makeup-input").val();
+  var type = "blush";
+console.log("makeup: " + makeup);
+// console.log("type: " + type);
   if (makeup !== "") {
     var temp = "brand=" + makeup;
     makeup = temp;
@@ -118,74 +124,50 @@ $("#searchButton").on("click", function () {
 
   //need to create model function that will add results
   queryURL += APIKey;
+  console.log(queryURL);
   getMakeupInfo(queryURL);
-
-  // $.ajax({
-  //     url: "https://dev.virtualearth.net/REST/v1/Imagery/Map/Road?pushpin=33.980499267578125,-84.21903991699219&pushpin=33.9613151550293,-84.12788391113281&format=jpeg&map&MapMetaData=0&key=AizLYVCVmDtzFe35OyVFF6FoMBjJuPA96Bc_pPQ50KQ9oMiNl4Pr89MbxB6FbzG9",
-  //     method: "GET"
-  // }).then(function(response) {
-  //     $("body").append($("<img>").attr("src", response));
-  // })
-  $("body").append(
-    $("<img>").attr(
-      "src",
-      "https://dev.virtualearth.net/REST/v1/Imagery/Map/Road?pushpin=33.980499267578125,-84.21903991699219&pushpin=33.9613151550293,-84.12788391113281&format=jpeg&map&MapMetaData=0&key=AizLYVCVmDtzFe35OyVFF6FoMBjJuPA96Bc_pPQ50KQ9oMiNl4Pr89MbxB6FbzG9"
-    )
-  );
-
-  // Microsoft.Maps.ConfigurableMap.createFromConfig(document.getElementById('myMap'), 'https://bingmapsisdk.blob.core.windows.net/isdksamples/configmap2.json', false, null, successCallback, errorCallback);
-  // function successCallback(mapObj) {
-  //     document.getElementById('printoutPanel').innerHTML = 'success';
-  // }
-  // function errorCallback(message) {
-  //     document.getElementById('printoutPanel').innerHTML = message;
-  // }
-
-  // var map = new Microsoft.Maps.Map(document.getElementById('myMap'), {});
-  // var pushpin = new Microsoft.Maps.Pushpin(map.getCenter(), null);
-  // map.entities.push(pushpin);
-  // pushpin.setOptions({ enableHoverStyle: true, enableClickedStyle: true });
 });
 
-//updated class to reflect "product" on html & below in order to have response work
-$(document).on("click", ".col-sm-12.product", function () {
-  var name = "red lipstick"; //$(this).attr("data-item");
 
-  var searchArray = name.split(" ");
-  searchItem = "";
-  for (var i = 0; i < searchArray.length; i++) {
-    if (i == searchArray.length - 1) {
-      searchItem += searchArray[i];
-    } else {
-      searchItem += searchArray[i] + "+";
-    }
-  }
-  console.log(searchItem);
-  // http://api.walmartlabs.com/v1/search?query=red+lipstick&format=json&apiKey=d7hjdvye4sky5cdwmmmtf3bf
-  var walmartURL =
-    "https://cors-anywhere.herokuapp.com/https://api.walmartlabs.com/v1/search?query=" +
-    searchItem +
-    "&format=json&apiKey=d7hjdvye4sky5cdwmmmtf3bf";
-  getWalmartInfo(walmartURL);
-  // createModal();
-});
-
-function showMakeupDetail(record) {
+// function showMakeupDetail(record) {
+  $(document).on("click", "#details", function() {
+    // if (event.matches("button")) {
+      var dataIndex = $(this).attr("data-index");
+      console.log(dataIndex);
+    // }
+      
+  
   //    Return anonymous function tied to record detail
-  return function () {
+
+  var makeupFromStorage = JSON.parse(localStorage.getItem("makeupObject"));
+  console.log(makeupFromStorage);
+
     var imgTag = $("<img>")
-      .attr("src", record.image_link)
+      .attr("src", makeupFromStorage[dataIndex].image_link)
       .attr("height", "30px")
       .attr("width", "30px")
       .attr("class", "images");
-    imageRow.append(imgTag);
+    $(".popup-content").append(imgTag);
     $(".popup-content").html("").append(imgTag);
+    var name = $("<div>").text("Name: " + makeupFromStorage[dataIndex].name);
+    $(".popup-content").append(name);
+    // var description = $("<div>").text("Description: " + makeupFromStorage[dataIndex].description);
+    // $(".popup-content").append(description);
+    var mapDiv = $("<div>").attr("id", "myMap");
+    mapDiv.css({
+      "position": "relative",
+      "height": "400px",
+      "width": "400px",
+      "left": "50%"
+    });
+    $(".popup-content").append(mapDiv);
+    getLocation();
     $(".popup, .popup-content").addClass("active");
     $(".btn.btn-secondary.btn-sm.close, .popup").on("click", function () {
       $(".popup, .popup-content").removeClass("active");
     });
-  };
-}
+    
+})
 
 function getMakeupInfo(queryURL) {
   //code here for ajax call and dynamic element creation
@@ -198,6 +180,7 @@ function getMakeupInfo(queryURL) {
     console.log(response[0].name);
     console.log(response[0].price);
     console.log(response[0].image_link);
+    localStorage.setItem("makeupObject", JSON.stringify(response));
 
     //can resize the for loop to however long we want
     for (var i = 0; i < 10; i++) {
@@ -212,7 +195,8 @@ function getMakeupInfo(queryURL) {
         .attr("src", response[i].image_link)
         .attr("height", "30px")
         .attr("width", "30px")
-        .attr("class", "images");
+        .attr("class", "images")
+        .attr("data-index", i);
       imageRow.append(imgTag);
       newCol.append(imageRow);
 
@@ -221,12 +205,14 @@ function getMakeupInfo(queryURL) {
         .addClass("row")
         .append($("<div>"))
         .addClass("col-sm-12 names")
+        .attr("data-index", i)
         .text("Name: " + response[i].name);
       newCol.append(nameRow);
       priceRow = $("<div>")
         .addClass("row")
         .append($("<div>"))
         .addClass("col-sm-12 prices")
+        .attr("data-index", i)
         .text("Price: " + response[i].price);
       newCol.append(priceRow);
       newRow.append(newCol);
@@ -235,22 +221,24 @@ function getMakeupInfo(queryURL) {
       buttonRow = $("<div>")
         .addClass("row")
         .append($("<div>"))
-        .addClass("col-sm-12");
-      var viewBtn = $("<input/>").attr({
-        type: "button",
-        class: "btn btn-secondary btn-sm",
-        id: "detail",
-        value: "view details",
-      });
+        .addClass("col-sm-12")
+        .attr("data-index", i);
+      var viewBtn = $("<button>")
+        .addClass("details btn btn-secondary btn-sm")
+        .attr("id", "details")
+        .attr("data-index", i)
+        .attr("type", "button")
+        .text("View Details");
       buttonRow.append(viewBtn);
       newCol.append(buttonRow);
 
       // creating an on click for modal pop-up to be triggered
 
-      viewBtn.on("click", showMakeupDetail(response[i]));
+      // viewBtn.on("click", showMakeupDetail(response[i]));
+      // $(document).on("click", "btn", showMakeupDetail(response));
 
       //appending to body, but can also append to a class or id
-      $("body").append(newRow);
+      $("#results-col").append(newRow);
     }
   });
 }
